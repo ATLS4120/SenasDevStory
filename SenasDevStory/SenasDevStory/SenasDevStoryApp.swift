1b146ce1016199ff82613f7ff0007018fd45d9e4
Sena Uctuk <seuc9452@colorado.edu>
2024-09-16 19:44:11 -0600

    Second Repo Set Up

---
+++ SenasDevStory/SenasDevStory/SenasDevStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenasDevStoryApp.swift
+//  SenasDevStory
+//
+//  Created by Sena Uctuk on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SenasDevStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
